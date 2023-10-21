5340d4d5016e6c092fa05ac0e86ea467fa36696e
davian27 <you@example.com>
2023-10-21 16:34:56 +0700

    Updated Landing Page + 1 Page Revisi 1

--- js/Index.js
+++ js/Index.js
@@ -1,1 +1,15 @@
 feather.replace();
+
+document.getElementById("masuk").addEventListener("click", function() {
+    var waktuSekarang = new Date();
+    var jam = waktuSekarang.getHours();
+    var menit = waktuSekarang.getMinutes();
+    var detik = waktuSekarang.getSeconds();
+
+
+    var formatwaktu = (jam < 10 ? "0" : "") + jam + ":" +
+                        (menit < 10 ? "0" : "") + menit +
+                        ":" + (detik < 10? "0" : "") + detik;
+
+    document.getElementById("jammasuk").value = formatwaktu;
+});
